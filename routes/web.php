9f5b61e2f29265624f4b97b4cf8b8ee254ed7c17
AreYouLucky <johnmichaelcagadas@gmail.com>
2026-08-08 08:56:23 +0800

    update on routes

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return inertia('Welcome');
-})->name('welcome');
-// require __DIR__.'/auth.php';
+require __DIR__.'/auth.php';
 require __DIR__.'/farmer.php';
 require __DIR__.'/processor.php';
 require __DIR__.'/lgu.php';
